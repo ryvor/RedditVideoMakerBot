c6b61c3aa05571818687b54e99f7d425baa0e3db
Robert Smith <master_schmadoodle@outlook.com>
2024-03-09 09:28:48 +0000

    Minor changes to fix multiple changes

--- video_creation/screenshot_downloader.py
+++ video_creation/screenshot_downloader.py
@@ -18,6 +18,45 @@ from utils.videos import save_data
 __all__ = ["download_screenshots_of_reddit_posts"]
 
 
+def set_preferred_theme(theme, page):
+    # Alternate method to try to set preferred theme
+    preferred_theme = 'dark' if theme == 'dark' else 'light'
+    dark_mode_switcher_loc = page.locator('faceplate-switch-input[value="darkmode-switch-value"]').first
+    if dark_mode_switcher_loc.count() == 1:
+        is_dark_mode_enabled = page.locator('html.theme-dark').first.count() > 0
+        if (preferred_theme == "dark" and not is_dark_mode_enabled) or (preferred_theme == "light" and is_dark_mode_enabled):
+            print("Try to set theme to " + (preferred_theme) + "...")
+            dark_mode_switcher_loc.dispatch_event('click')
+            # Ensure to set preferred theme
+            page.wait_for_function("""
+                preferred_theme => {
+                    if (!document.querySelector('html').classList.contains('theme-' + preferred_theme)) {
+                        document.querySelector('faceplate-switch-input[value="darkmode-switch-value"]').click();
+                    }
+                    return true;
+                }
+            """, arg=preferred_theme)
+            # breakpoint()
+
+def bypass_see_this_post_in(page):
+    # Bypass "See this post in..."
+    see_this_post_in_button = page.locator('#bottom-sheet button.continue').first
+    if see_this_post_in_button.is_visible():
+        print("See this post in... [CONTINUE]")
+        see_this_post_in_button.dispatch_event('click')
+        see_this_post_in_button.wait_for(state='hidden')
+    else:
+        # Ensure to hide backdrop
+        backdrop_loc = page.locator('#bottom-sheet #backdrop').first
+        if backdrop_loc.count() > 0:
+            backdrop_loc.evaluate('node => node.style.display="none"')
+
+def get_comment_excerpt(comment):
+    comment_excerpt = (comment["comment_body"].split("\n")[0])
+    if len(comment_excerpt) > 80: comment_excerpt = comment_excerpt[:80] + "…"
+
+    return comment_excerpt
+
 def get_screenshots_of_reddit_posts(reddit_object: dict, screenshot_num: int):
     """Downloads screenshots of reddit posts as seen on the web. Downloads to assets/temp/png
 
@@ -75,19 +114,24 @@ def get_screenshots_of_reddit_posts(reddit_object: dict, screenshot_num: int):
         print_substep("Launching Headless Browser...")
 
         browser = p.chromium.launch(
-            headless=True
+            headless=False
         )  # headless=False will show the browser for debugging purposes
         # Device scale factor (or dsf for short) allows us to increase the resolution of the screenshots
         # When the dsf is 1, the width of the screenshot is 600 pixels
         # so we need a dsf such that the width of the screenshot is greater than the final resolution of the video
         dsf = (W // 600) + 1
 
+        # User Agent
+        ua = "Mozilla/5.0 (Linux; Android 8.0.0; MI 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.101 Mobile Safari/537.36"
+
         context = browser.new_context(
             locale=lang or "en-us",
             color_scheme="dark",
             viewport=ViewportSize(width=W, height=H),
             device_scale_factor=dsf,
+            user_agent=ua,
         )
+
         cookies = json.load(cookie_file)
         cookie_file.close()
 
@@ -96,45 +140,60 @@ def get_screenshots_of_reddit_posts(reddit_object: dict, screenshot_num: int):
         # Login to Reddit
         print_substep("Logging in to Reddit...")
         page = context.new_page()
-        page.goto("https://www.reddit.com/login", timeout=0)
-        page.set_viewport_size(ViewportSize(width=1920, height=1080))
-        page.wait_for_load_state()
+        
+        screenshot_debug = True
+        
+        # Use old.reddit.com to login only (go to reddit.com for actual posts/comments later)
+        page.goto("https://old.reddit.com/login", timeout=0)
+        # page.set_viewport_size(ViewportSize(width=1920, height=1080))
+        page.set_viewport_size(ViewportSize(width=1200, height=720))
+        login_url = page.url
 
-        page.locator('[name="username"]').fill(settings.config["reddit"]["creds"]["username"])
-        page.locator('[name="password"]').fill(settings.config["reddit"]["creds"]["password"])
-        page.locator("button[class$='m-full-width']").click()
-        page.wait_for_timeout(5000)
+        username_loc = page.locator("#login-form #user_login").first
+        password_loc = page.locator("#login-form #passwd_login").first
+        button_loc = page.locator("#login-form button[type='submit']").first
 
-        login_error_div = page.locator(".AnimatedForm__errorMessage").first
-        if login_error_div.is_visible():
-            login_error_message = login_error_div.inner_text()
-            if login_error_message.strip() == "":
-                # The div element is empty, no error
-                pass
-            else:
-                # The div contains an error message
-                print_substep(
-                    "Your reddit credentials are incorrect! Please modify them accordingly in the config.toml file.",
-                    style="red",
-                )
-                exit()
+        print("Logging in via old.reddit.com/login...")
+        username_loc.fill(settings.config["reddit"]["creds"]["username"])
+        password_loc.fill(settings.config["reddit"]["creds"]["password"])
+        button_loc.first.click()
+        
+        # Check for login error message
+        login_error_loc = page.locator("#login-form .c-form-control-feedback-error").first
+        if login_error_loc.is_visible():
+            print_substep(
+                "Login unsuccessful: probably your reddit credentials are incorrect! Please modify them accordingly in the config.toml file.",
+                style="red",
+            )
+            exit()
+
+        # Wait for navigation to page different from the login one
+        not_login_url_regex = re.compile('^(?!' + login_url + ')')
+        page.wait_for_url(not_login_url_regex, wait_until="commit") # wait_until='commit' -> wait until another url started loading
+
+        current_url = page.url
+        if current_url == "https://old.reddit.com/":
+            print("Login successful!")
         else:
-            pass
+            print_substep(
+                "Login unsuccessful: probably your reddit credentials are incorrect! Please modify them accordingly in the config.toml file.",
+                style="red",
+            )
+            exit()
 
-        page.wait_for_load_state()
-        # Handle the redesign
-        # Check if the redesign optout cookie is set
-        if page.locator("#redesign-beta-optin-btn").is_visible():
-            # Clear the redesign optout cookie
-            clear_cookie_by_name(context, "redesign_optout")
-            # Reload the page for the redesign to take effect
-            page.reload()
-        # Get the thread screenshot
+        # Goto thread url
         page.goto(reddit_object["thread_url"], timeout=0)
         page.set_viewport_size(ViewportSize(width=W, height=H))
+        page.set_viewport_size(ViewportSize(width=1200, height=720))
         page.wait_for_load_state()
         page.wait_for_timeout(5000)
 
+        # Try to set preferred theme from settings
+        set_preferred_theme(settings.config["settings"]["theme"], page)
+
+        # Bypass "See this post in..."
+        bypass_see_this_post_in(page)
+        
         if page.locator(
             "#t3_12hmbug > div > div._3xX726aBn29LDbsDtzr_6E._1Ap4F5maDtT1E1YuCiaO0r.D3IL3FD0RFy_mkKLPwL4 > div > div > button"
         ).is_visible():
@@ -171,18 +230,23 @@ def get_screenshots_of_reddit_posts(reddit_object: dict, screenshot_num: int):
 
         postcontentpath = f"assets/temp/{reddit_id}/png/title.png"
         try:
+            post_loc = page.locator("shreddit-post")
+
+            # Bypass "See this post in..."
+            bypass_see_this_post_in(page)
+
             if settings.config["settings"]["zoom"] != 1:
                 # store zoom settings
                 zoom = settings.config["settings"]["zoom"]
                 # zoom the body of the page
                 page.evaluate("document.body.style.zoom=" + str(zoom))
                 # as zooming the body doesn't change the properties of the divs, we need to adjust for the zoom
-                location = page.locator('[data-test-id="post-content"]').bounding_box()
-                for i in location:
-                    location[i] = float("{:.2f}".format(location[i] * zoom))
-                page.screenshot(clip=location, path=postcontentpath)
+                post_loc = post_loc.bounding_box()
+                for i in post_loc:
+                    post_loc[i] = float("{:.2f}".format(post_loc[i] * zoom))
+                page.screenshot(clip=post_loc, path=postcontentpath)
             else:
-                page.locator('[data-test-id="post-content"]').screenshot(path=postcontentpath)
+                post_loc.first.screenshot(path=postcontentpath)
         except Exception as e:
             print_substep("Something went wrong!", style="red")
             resp = input(
@@ -208,10 +272,11 @@ def get_screenshots_of_reddit_posts(reddit_object: dict, screenshot_num: int):
             )
         else:
             for idx, comment in enumerate(
-                track(
-                    reddit_object["comments"][:screenshot_num],
-                    "Downloading screenshots...",
-                )
+                reddit_object["comments"][:screenshot_num]
+                # track(
+                    # reddit_object["comments"][:screenshot_num],
+                    # "Downloading screenshots...",
+                # )
             ):
                 # Stop if we have reached the screenshot_num
                 if idx >= screenshot_num:
@@ -221,6 +286,13 @@ def get_screenshots_of_reddit_posts(reddit_object: dict, screenshot_num: int):
                     page.locator('[data-testid="content-gate"] button').click()
 
                 page.goto(f'https://reddit.com{comment["comment_url"]}', timeout=0)
+                
+                # Try to set preferred theme from settings
+                set_preferred_theme(settings.config["settings"]["theme"], page)
+
+                if screenshot_debug:
+                    comment_excerpt = get_comment_excerpt(comment)
+                    print(f"[{idx + 1}/{screenshot_num} {comment['comment_id']}]: {comment_excerpt}")
 
                 # translate code
 
@@ -235,23 +307,49 @@ def get_screenshots_of_reddit_posts(reddit_object: dict, screenshot_num: int):
                         [comment_tl, comment["comment_id"]],
                     )
                 try:
+                    comment_selector = f'shreddit-comment[thingid="t1_{comment["comment_id"]}"]'
+                    comment_loc = page.locator(comment_selector)
                     if settings.config["settings"]["zoom"] != 1:
                         # store zoom settings
                         zoom = settings.config["settings"]["zoom"]
                         # zoom the body of the page
                         page.evaluate("document.body.style.zoom=" + str(zoom))
                         # scroll comment into view
-                        page.locator(f"#t1_{comment['comment_id']}").scroll_into_view_if_needed()
+                        comment_loc.scroll_into_view_if_needed()
                         # as zooming the body doesn't change the properties of the divs, we need to adjust for the zoom
-                        location = page.locator(f"#t1_{comment['comment_id']}").bounding_box()
+                        comment_loc.bounding_box()
                         for i in location:
                             location[i] = float("{:.2f}".format(location[i] * zoom))
                         page.screenshot(
-                            clip=location,
+                            clip=comment_loc,
                             path=f"assets/temp/{reddit_id}/png/comment_{idx}.png",
                         )
                     else:
-                        page.locator(f"#t1_{comment['comment_id']}").screenshot(
+                        # Bypass "See this post in..."
+                        bypass_see_this_post_in(page)
+
+                        # Click on "View more comments", if present
+                        view_more_comments_button = page.locator('.overflow-actions-dialog ~ button').first
+                        if view_more_comments_button.is_visible():
+                            print("View more comments... [CLICK]")
+                            view_more_comments_button.dispatch_event('click')
+                            view_more_comments_button.wait_for(state='hidden')
+
+                        # If the comment text itself is collapsed, expand it
+                        comment_text_loc = comment_loc.locator("p").first
+                        if not comment_text_loc.is_visible():
+                            self_expand_button_loc = comment_loc.locator('summary button').first
+                            if self_expand_button_loc.is_visible():
+                                self_expand_button_loc.dispatch_event('click')
+
+                        # If replies are expanded toggle them
+                        expanded_loc = comment_loc.locator('button[aria-expanded="true"]').first
+                        if expanded_loc.is_visible():
+                            #print("If replies are expanded toggle them")
+                            expanded_loc.dispatch_event("click")
+
+                        # breakpoint()
+                        comment_loc.first.screenshot(
                             path=f"assets/temp/{reddit_id}/png/comment_{idx}.png"
                         )
                 except TimeoutError:
